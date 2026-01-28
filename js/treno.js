162bada6a8a75e62557f30cdce6a6d2856482f46
majk.mone <mone.majk@gmail.com>
2026-01-28 09:02:01 +0100

    edit price discount

--- js/treno.js
+++ js/treno.js
@@ -5,7 +5,7 @@ let pricePerKm = 0.21
 if (age < 18) {
 	pricePerKm = pricePerKm - (pricePerKm * 20) / 100
 } else if (age > 65) {
-	pricePerKm = pricePerKm - (pricePerKm * 20) / 100
+	pricePerKm = pricePerKm - (pricePerKm * 40) / 100
 }
 
 let ticketPrice = (Math.round(km * pricePerKm * 100) / 100).toFixed(2)
